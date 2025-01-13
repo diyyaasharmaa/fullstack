49462462b13fb5c0072e9cef4e6604e9c94a6f04
diyyaasharmaa <diya.sharma.al23@ggits.net>
2025-01-13 15:46:25 +0530

    code

---
+++ Lec18/javascript.js
@@ -0,0 +1,40 @@
+console.log('heeheheheee');
+
+// datatypes:
+// Number
+// a=5.5
+// String 'c' 'cc' "ccc"
+// boolean true, false,0,1
+// null
+// umdefined
+// Symbol
+// BigInt
+  
+// keywords
+// var, let, const
+// var a=5
+// console.log(a);
+// var a=5
+// var a=100
+// console.log(a);
+// let a=5
+// a=100
+// console.log(a);
+// const a=5
+// a=3000
+// console.log(a);
+
+// console.log(0.1+0.2=0.3);
+
+// var a=100
+// var to=a==100
+// console.log(to);
+
+// let a='rahul'
+// let b='jain'
+// console.log(`${a}   ${b}`);
+
+
+ 
+
+
