6443270fc4bd55a533eb3c92a493543e62b6ff73
diyyaasharmaa <diya.sharma.al23@ggits.net>
2025-01-25 19:48:19 +0530

    js

---
+++ Lec26/dom.js
@@ -0,0 +1,22 @@
+// DOM- Document Object Model
+// html+js
+
+// console.log(hello); =>undefined
+
+// console.log(document);
+
+let data= document.getElementsByClassName('two')
+// let data2= document.getElementById('one')
+// console.log(data[0].innerHTML,'dataaa');
+
+// data.style.color='red'
+data[0].style.color='red'
+
+// for(let i of data){
+//     i.style.color='red'
+// }
+
+
+
+
+
