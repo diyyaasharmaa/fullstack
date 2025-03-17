faf4e34277d81834cea466b0dff315ccef014a47
diyyaasharmaa <diya.sharma.al23@ggits.net>
2025-03-17 18:02:47 +0530

    js

---
+++ practice/index.js
@@ -0,0 +1,73 @@
+// Pascal Case
+// var SmallNumber= 90
+// Snake case
+// var small_number= 10
+// camel Case 
+// var smallNumberWith = 20
+
+// reassignment is possible
+// var small_number= 10 
+// small_number=9
+// console.log(small_number)
+
+// reassignment is not possible
+// const other=11
+// other=12
+// console.log(other)
+
+// redeclaration is not possible
+// let another=22
+// another=65
+// console.log(another)
+
+// Arithematic op 
+// const a=10
+// const b=20
+// const c=30
+// console.log(a+c+b)
+// console.log(a-c-b)
+// console.log(a/c)
+// console.log(a%c)
+// console.log(b*c*a);
+
+
+//FizzBuzz problem
+
+// const num=15
+// if(num % 3 ==0 && num%5 == 0){
+//     console.log("FizzBuzz");
+// }
+// else if(num%3==0){
+//     console.log("Fizz");
+// }
+// else if(num%5==0){
+//     console.log("Buzz");
+// }
+// else{
+//     console.log("Not divisible by either of them");
+    
+// }
+
+//Loops- for , while, do while
+
+//for
+
+// for(let i=0; i<=100;i++){
+//     if(i % 3 === 0 && i % 5 === 0 ){
+//         console.log(i);
+//     }
+// }
+
+//while
+
+// while(condition){}
+
+let num=1
+while(num<10){
+    console.log(num, "*", (10-num));
+num++;
+}
+
+
+
+
